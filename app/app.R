29d845dd08169bf6365cc7e0beda77688d01ae8d
Jeremy Weiss <jlweiss@email.arizona.edu>
2026-04-15 16:35:44 -0700

    select input-locator pin div

--- app/app.R
+++ app/app.R
@@ -30,13 +30,43 @@ ui <-
     azmetInRealTimeMobile = 
       
       bslib::page_fillable(
-        shiny::actionButton(
-          inputId = "open",
-          label = span(
-            bsicons::bs_icon("geo-alt"),
-            textOutput("selected_station", container = span)
+        title = NULL,
+        theme = theme, # `scr##_theme.R`
+        
+        # htmltools::tags$head(htmltools::includeHTML("www/pwa/pwa.html")),
+        
+        htmltools::p(
+          bsicons::bs_icon("sliders", class = "bolder-icon"), 
+          htmltools::HTML("&nbsp;<strong>DATA OPTIONS</strong>&nbsp;"),
+          bslib::tooltip(
+            bsicons::bs_icon("info-circle"),
+            "Specify an AZMet station or tap on the location pin to select the nearest one.",
+            id = "infoDataOptions",
+            placement = "right"
           ),
-          class = "btn-primary btn-m"
+          
+          class = "data-options-title"
+        ),
+        
+        htmltools::div(
+          class = "download-buttons-div",
+          style = "display: flex; align-items: center; column-gap: 1.5rem;", # Flexbox styling
+          
+          shiny::selectInput(
+            inputId = "azmetStation", 
+            label = "AZMet Station",
+            choices = c("Group 1", "Group 2", "Group 3", "Group 4", "Group 5", "Group 6"),
+            selected = c("Group 1", "Group 2", "Group 3", "Group 4", "Group 5", "Group 6")[1]
+          ),
+          
+          shiny::actionButton(
+            inputId = "open",
+            label = span(
+              bsicons::bs_icon("geo-alt", class = "bolder-icon geoloc"),
+              textOutput("selected_station", container = span)
+            ),
+            class = "btn btn-default btn-blue geoloc"
+          )
         )
       )
   ) # htmltools::htmlTemplate()
